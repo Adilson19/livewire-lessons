55c55f72d0376da32b6aa2b9228907b991723683
Adilson19 <adilsonsousaas82@gmail.com>
2025-07-07 09:42:23 +0100

    feat: exemplo da aplicacao dos livewires no back-end com influencia no front-end

--- app/Livewire/Counter.php
+++ app/Livewire/Counter.php
@@ -11,12 +11,26 @@ class Counter extends Component
         return view('livewire.counter');
     }
 
-    public $number = 0;
-
-    //  Inves de duas funcoes, podemos ter uma unica funcao que recebe o valor a ser incrementado
+    //public $number = 0;
+    /*  Inves de duas funcoes, podemos ter uma unica funcao que recebe o valor a ser incrementado
     public function increment($quantity)
     {
         $this->number = $this->number + $quantity;
+    }*/
+
+    // Estamos criando duas funcoes, uma para incrementar e outra para decrementar
+    public $number = 0;
+    public function increment()
+    {
+        // Ele vai incrementar ate que que seja menor do que 5
+        if($this->number < 5){
+            $this->number = $this->number + 1;
+        }
+    }
+
+    public function decrement()
+    {
+        $this->number  = $this->number - 1;
     }
 }
  
--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,6 +1,9 @@
 <div>
     <h1>Contador: {{ $number }}</h1>
     {{-- Para inves de usar variaveis ele pega diretamente os elementos que estao na pagina --}}
-    <button wire:mouseenter="increment($event.target.innerText)">1</button>
-    <button wire:click="increment($event.target.innerText)">2</button>
+    {{-- <button wire:mouseenter="increment($event.target.innerText)">1</button>
+    <button wire:click="increment($event.target.innerText)">2</button> --}}
+
+    <button wire:click="increment">+</button>
+    <button wire:click="decrement">-</button>
 </div>
